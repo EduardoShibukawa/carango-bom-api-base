06cda8dd7b33a0803ff37293c81097b441135c88
Lucas Carvalho <lucasbr023@gmail.com>
2021-07-01 16:36:14 -0300

    fix dashboard tests

--- src/test/java/br/com/caelum/carangobom/services/DashboardServiceTest.java
+++ src/test/java/br/com/caelum/carangobom/services/DashboardServiceTest.java
@@ -1,25 +1,20 @@
 package br.com.caelum.carangobom.services;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
-
-import java.math.BigDecimal;
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-
 import br.com.caelum.carangobom.domain.Brand;
 import br.com.caelum.carangobom.domain.Car;
 import br.com.caelum.carangobom.dtos.CarPriceByBrandItemResponse;
 import br.com.caelum.carangobom.repositories.CarRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 class DashboardServiceTest {
 
@@ -68,17 +63,17 @@ class DashboardServiceTest {
         		allOf(
     				hasProperty("brand", is("Fiat")), 
     				hasProperty("count", is(3L)),
-    				hasProperty("total", is(BigDecimal.valueOf(16000)))
+    				hasProperty("totalPrice", is(BigDecimal.valueOf(16000)))
 				),
         		allOf(
     				hasProperty("brand", is("Audi")), 
     				hasProperty("count", is(1L)),
-    				hasProperty("total", is(BigDecimal.valueOf(150000)))
+    				hasProperty("totalPrice", is(BigDecimal.valueOf(150000)))
         		),
         		allOf(
     				hasProperty("brand", is("Ford")), 
     				hasProperty("count", is(1L)),
-    				hasProperty("total", is(BigDecimal.valueOf(10000)))
+    				hasProperty("totalPrice", is(BigDecimal.valueOf(10000)))
 				)
 		));
 	}
